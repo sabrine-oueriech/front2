847c99cc99f061557fc3539c7ebdafef80c36152
ichrak <ichrakneji0@gmail.com>
2024-03-07 13:00:38 +0100

    troisieme modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Importez votre composant Home
 import { ContactComponent } from './contact/contact.component'; // Importez votre composant Contact
 import { ChoixComponent } from './choix/choix.component'; // Importez votre composant Contact
+import { FormComponent } from './form/form.component'; // Importez le composant FormComponent
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // Route pour la page d'accueil
+  { path: 'home', component: HomeComponent },
+  // Route pour la page d'accueil
   { path: 'contact', component: ContactComponent }, 
-  { path: 'choix', component: ChoixComponent }, // Ajoutez cette ligne pour le composant Choix
+  { path: 'choix', component: ChoixComponent }, 
+  { path: 'form', component: FormComponent }, // Ajoutez cette ligne pour le composant Form
+  // Ajoutez cette ligne pour le composant Choix
 ];
 
 @NgModule({
--- src/app/choix/choix.component.ts
+++ src/app/choix/choix.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-choix',
   templateUrl: './choix.component.html',
-  styleUrl: './choix.component.css'
+  styleUrls: ['./choix.component.css']
 })
 export class ChoixComponent {
+
+  constructor(private router: Router) {}
+
+  afficherForm() {
+    this.router.navigate(['/form']);
+  }
 
 }
